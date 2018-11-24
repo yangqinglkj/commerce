13bdefa0bee6e69ac7a9544952fc2f922b9ab9b4
yangqing <137118752@qq.com>
2018-11-24 16:58:20 +0800

    第6次提交

--- pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/ItemCatController.java
+++ pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/ItemCatController.java
@@ -75,31 +75,9 @@ public class ItemCatController {
 		}
 	}
 
-	/**
-	 * 修改
-	 * @param itemCat
-	 * @return
-	 */
-	@RequestMapping("/update2")
-	public Result update2(@RequestBody TbItemCat itemCat){
-		try {
-			itemCatService.update(itemCat);
-			return new Result(true, "修改成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "修改失败");
-		}
-	}
-	@RequestMapping("/update3")
-	public Result update3(@RequestBody TbItemCat itemCat){
-		try {
-			itemCatService.update(itemCat);
-			return new Result(true, "修改成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "修改失败");
-		}
-	}
+
+
+
 	/**
 	 * 获取实体
 	 * @param id
